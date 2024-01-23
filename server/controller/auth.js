1532fb8aa712ba39321a05c4b6cf0e5d9215622f
KostsPlayer <azka.nuril070@gmail.com>
2024-01-24 06:55:54 +0700

    1st after trial

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -1,11 +1,62 @@
 import express from "express";
-import bcrypt from "bcrypt";
 import supabase from "../config/supabase.js";
+import bcrypt from "bcrypt";
 import configureMiddleware from "../config/middleware.js";
 
 const app = express();
 configureMiddleware(app);
 const router = express.Router();
+
+router.post("/registration", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const saltRound = 10;
+    const hash = await bcrypt.hash(password, saltRound);
+
+    const { data, error } = await supabase
+      .from("users")
+      .insert({ name: username, email: email, password: hash })
+      .select("*");
+
+    if (error) {
+      return res.json(error.message);
+    }
+
+    const userId = data[0].id;
+    return res.json({ success: true, userId: userId, data });
+  } catch (error) {
+    return res.json(error);
+  }
+});
+
+router.post("/assign-role", async (req, res) => {
+  try {
+    const { userId, role } = req.body;
+
+    let roleValue;
+
+    if (role === "Seller") {
+      roleValue = 2;
+    } else {
+      roleValue = 3;
+    }
+
+    const { data, error } = await supabase
+      .from("users")
+      .update({ role_id: roleValue })
+      .eq("id", userId)
+      .select("*");
+
+    if (error) {
+      return res.json(error.message);
+    }
+
+    return res.json(data);
+  } catch (error) {
+    return res.json(error);
+  }
+});
 
 router.post("/login", async (req, res) => {
   const { usernameEmail, password } = req.body;
--- server/index.js
+++ server/index.js
@@ -1,8 +1,5 @@
 import express from "express";
 import configureMiddleware from "./config/middleware.js";
-import signupRouter from "./controller/signup.js";
-import loginRouter from "./controller/login.js";
-import roleRouter from "./controller/roles.js";
 import menuRouter from "./controller/menu.js";
 import productRouter from "./controller/product.js";
 import cartRouter from "./controller/cart.js";
@@ -10,6 +7,7 @@ import orderRouter from "./controller/order.js";
 import userRouter from "./controller/users.js";
 import categoryRouter from "./controller/category.js";
 import addressesRouter from "./controller/addresses.js";
+import authRouter from "./controller/auth.js";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -17,9 +15,6 @@ dotenv.config();
 const app = express();
 configureMiddleware(app);
 
-app.use(loginRouter);
-app.use(signupRouter);
-app.use(roleRouter);
 app.use(menuRouter);
 app.use(productRouter);
 app.use(cartRouter);
@@ -27,6 +22,7 @@ app.use(orderRouter);
 app.use(userRouter);
 app.use(categoryRouter);
 app.use(addressesRouter);
+app.use(authRouter);
 
 const port = process.env.REACT_APP_PORT || 5001;
 app.listen(port, () => {
