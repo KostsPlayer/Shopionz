c74806c8ce89e3829ff10106ef41b3b6a74ef938
KostsPlayer <azka.nuril070@gmail.com>
2024-05-06 20:52:02 +0700

    5th after trial

--- server/config/supabase.js
+++ server/config/supabase.js
@@ -3,7 +3,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const supabaseUrl = process.env.SUPABASE_URL;
+// const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseUrl =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNyaWp0a2J2bW1wamRieHFxa3BpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDMwMzkzNDcsImV4cCI6MjAxODYxNTM0N30.iO8nGzYD2kA0saijD8uHCYrTQj-U1Cf3XDaSNx7rzaA";
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
